2e2a60ce339c6c4331bb0f3dd7adeffd74f1f2d7
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-09-21 20:00:14 -0700

    second resubmit

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ describe( 'greet', function(){
   describe('hello', function() {
     it('should return null with non string parameters', function(){
       let result = greet.name('');
-      expect(result).toBe('provide name please');
+      expect(result).toBe(null);
     });
   });
 });
@@ -19,7 +19,7 @@ describe( 'greet', function(){
   describe('hello world', function() {
     it('should return hello world', function(){
       let result = greet.name('world');
-      expect(result).toBe('hello world');
+      expect(result).toBe('Hello world');
     });
   });
 });
@@ -39,7 +39,7 @@ describe( 'arithmetic', function(){
   describe('add correct', function() {
     it('should return sum of a or b ', function(){
       let result = arithmetic.add(4, 5);
-      expect(result).toBe(9);
+      expect(result).toEqual(9);
     });
   });
 });
